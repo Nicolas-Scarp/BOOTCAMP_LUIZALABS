d5ba640f098117f1c1a489abaa40f42208c853a8
Nicolas Scarp <contatonicolasscarp@gmail.com>
2026-04-18 09:54:05 -0300

    feature: updating

--- Módulo_2/defs/__init__.py
+++ Módulo_2/defs/__init__.py
@@ -1,20 +1,26 @@
-class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor):
-        self.cor = input("Informe a cor da bicicleta: ")
-        self.modelo = input("Informe o modelo da bicicleta: ")
-        self.ano = input("Informe o ano da bicicleta: ")
-        self.valor = input("Informe o valor da bicicleta: ")
+def bike():
+    class Bicicleta:
+        def __init__(self, cor, modelo, ano, valor):
+            self.cor = input("Informe a cor da bicicleta: ")
+            self.modelo = input("Informe o modelo da bicicleta: ")
+            self.ano = input("Informe o ano da bicicleta: ")
+            self.valor = input("Informe o valor da bicicleta: ")
+        
+        def buzinar(self):
+            print("\nPlim Plim...")
+        
+        def parar(self):
+            print("\nParando bicicleta...")
+            print("Bicicleta parada!")
     
-    def buzinar(self):
-        print("Plim Plim...")
-    
-    def parar(self):
-        print("Parando bicicleta...")
-        print("Bicicleta parada!")
- 
-    def correr(self):
-        print("Vrummmmm...")
+        def correr(self):
+            print("\nVrummmmm...")
 
+        def __str__(self):
+            return f"Bicicleta: {self.cor}, {self.modelo}, {self.ano}, {self.valor}"
+    
+
+<<<<<<< Updated upstream
     def __str__(self):
         return f"Bicicleta: {self.cor}, {self.modelo}, {self.ano}, {self.valor}"
  
@@ -28,17 +34,27 @@ fun ={
 }
 
 
+=======
+    b1 = Bicicleta("cor","modelo","ano","valor")
 
-print(f"\nA bicicleta tem a cor {b1.cor}, é do modelo {b1.modelo}, do ano de {b1.ano}, no valor de R${b1.valor}")
+    fun ={
+        "CORRER":b1.correr,
+        "PARAR":b1.parar,
+        "BUZINAR":b1.buzinar
+    }
 
-print("\n================================")
-print("Faça a sua escolha!!!")
-print("Correr | Buzinar | Parar")
-print("================================")
+    print(f"\nA bicicleta tem a cor {b1.cor}, é do modelo {b1.modelo}, do ano de {b1.ano}, no valor de R${b1.valor}")
+>>>>>>> Stashed changes
+
+    print("\n================================")
+    print("Faça a sua escolha!!!")
+    print("Correr | Buzinar | Parar")
+    print("================================")
 
 while True:
     func = input("O que deseja fazer? ").upper()
 
+<<<<<<< Updated upstream
     if func == "EXIT":
         break
 
@@ -48,4 +64,19 @@ while True:
         escolha()
     else:
         print("Valor inválido!")
+=======
+    while True:
+        func = input("\n").upper()
+        if func in fun:
+            fun[func]()
+        elif func == "EXIT":
+            break
+        else:
+            print("Comando Inválido!")
 
+def wait():
+    print("wait a moment")
+>>>>>>> Stashed changes
+
+def wait2():
+    print("wait a moment 2")
---
+++ Módulo_2/main.py
@@ -0,0 +1,7 @@
+import defs
+
+dict = {
+    "1":defs.bike,
+    "2":defs.wait,
+    "3":defs.wait2
+}
