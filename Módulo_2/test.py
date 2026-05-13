e3d8c67a4959d8d890e43fc964c9f66ec8d67d43
Nicolas Scarp <contatonicolasscarp@gmail.com>
2026-05-13 16:38:27 -0300

    Feature: Update Code.

--- Módulo_2/test.py
+++ Módulo_2/test.py
@@ -1,12 +1,17 @@
 class Estudante:
-    escola = "DIO"
+    escola = input("Digite a escola: ").upper()
 
     def __init__(self,nome,matricula):
         self.nome = nome
         self.matricula = matricula
-
+        
     def __str__(self) -> str:
         return f"{self.nome} - {self.matricula} - {self.escola}"
+    
+    def cumprimento(self):
+        return f"Meu nome é {self.nome} e eu estudo na {self.escola} e minha matricula é {self.matricula}"
 
 aluno_1 = Estudante("Nicolas",2107)
 print(aluno_1)
+
+
